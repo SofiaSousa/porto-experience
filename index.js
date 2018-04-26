2ca6430f05eaa654bbf1cd2a0655e9c8bac68213
Sofia Sousa <csofiamsousa@gmail.com>
2018-04-26 22:44:34 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,3 @@
+alert( 'Hello, Porto!' );
+alert( 'Dev Team Note: none of us have experience with this technology, so we expect developer Rodolfo might help us with this task...' );
+alert( 'Good luck mate Rodolfo! :D' );
